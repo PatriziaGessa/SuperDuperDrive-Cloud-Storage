75af4b7c134ca2d86405d0186112c158384e9c23
PatriziaGessa <patriziagessa@gmail.com>
2021-10-31 09:41:41 +0100

    Udacity Project

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/selenium/HomePage.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-//TODO check the another codes
-// - check the difference between the codes with the GitHub -> sakshee-19
+
 
 
 public class HomePage {
